404bd145a52a84a55ae3cf5d36465a2cd2132e8e
bablukpik@gmail.com <bablukpik@gmail.com>
2017-05-14 01:34:19 +0600

    Bd Map added

---
+++ BDmap/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>asdfasd</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div id="container"></div>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://code.highcharts.com/maps/highmaps.js"></script>
+	<script src="https://code.highcharts.com/maps/modules/exporting.js"></script>
+	<script src="https://code.highcharts.com/mapdata/countries/bd/bd-all.js"></script>
+	<script src="myJS.js"></script>
+</body>
+</html>
