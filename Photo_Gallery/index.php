1bf45e9877f743a30fe236b41387c49ec93d4380
bablukpik <bablukpik@gmail.com>
2017-08-17 11:38:24 +0600

    Added photo Gallery

---
+++ Photo_Gallery/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "">
+<html xmlns="" dir="ltr" lang="en-US">
+	<head profile="">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+		<title>Test</title>
+		
+		
+		
+		<link rel='stylesheet' id='NextGEN-css'  href='g_css/nggallery8a54.css?ver=1.0.0' type='text/css' media='screen' />
+		<link rel='stylesheet' id='thickbox-css'  href='g_css/thickboxccfb.css?ver=3.4.2' type='text/css' media='all' />
+		<script type='text/javascript' src='g_css/jqueryba3a.js?ver=1.7.2'></script>
+		
+	</head>
+	<body>
+	
+					
+
+												
+										    <!-- Thumbnails -->
+
+                                                									
+
+												<div id="ngg-image-184" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/1st_dist_meet_1.jpg" title="The 1st District Committee Meeting of 2013-14 was held on 14th September, 2013 at RAOWA Club, Dhaka. DC Hosne Ara Chowdhury Preside the meeting. Reports by the Dist. Officials & the election of NR & DNR for 2014-15 were the two important event of the meeting. Akhter Jahan as NR & Suraiya Alam as DNR was elected for next year." class="thickbox" rel="set_9" > <img title="1" alt="1" src="pg_image/1st_dist_meet_1.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+
+												<div id="ngg-image-178" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/1st_dist_meet_2.jpg" title="The 1st District Committee Meeting of 2013-14 was held on 14th September, 2013 at RAOWA Club, Dhaka. DC Hosne Ara Chowdhury Preside the meeting. Reports by the Dist. Officials & the election of NR & DNR for 2014-15 were the two important event of the meeting. Akhter Jahan as NR & Suraiya Alam as DNR was elected for next year." class="thickbox" rel="set_9" > <img title="24" alt="24" src="pg_image/1st_dist_meet_2.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+
+												<div id="ngg-image-177" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/1st_dist_meet_3.jpg" title="The 1st District Committee Meeting of 2013-14 was held on 14th September, 2013 at RAOWA Club, Dhaka. DC Hosne Ara Chowdhury Preside the meeting. Reports by the Dist. Officials & the election of NR & DNR for 2014-15 were the two important event of the meeting. Akhter Jahan as NR & Suraiya Alam as DNR was elected for next year." class="thickbox" rel="set_9" > <img title="23" alt="23" src="pg_image/1st_dist_meet_3.jpg"g" width="120" height="90" /> </a>													</div>
+						    </div>
+
+												<div id="ngg-image-176" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/dist_assembly1.jpg" title="28th Dist. Assembly of “We for women 0f today & tomorrow” was held on June 29, 2013 at Palm View Restaurent, Uttara, Dhaka. Eminent personality Syed Shamsul Haq was the Chief Guest. DC 0f 2012-13 Suraiya Alam preside over the meeting. The programme was hosted by IWC of Dhaka North East. The new  team of 2013-14 were warmly welcomed by the outgoing team of 2012-13. IW members from different districts & Rotarians participation make the programe joyous. The Assembly ended by the recognition of Clubs." class="thickbox" rel="set_9" > <img title="22" alt="22" src="pg_image/dist_assembly1.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+
+												<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/dist_assembly2.jpg" title="28th Dist. Assembly of “We for women 0f today & tomorrow” was held on June 29, 2013 at Palm View Restaurent, Uttara, Dhaka. Eminent personality Syed Shamsul Haq was the Chief Guest. DC 0f 2012-13 Suraiya Alam preside over the meeting. The programme was hosted by IWC of Dhaka North East. The new  team of 2013-14 were warmly welcomed by the outgoing team of 2012-13. IW members from different districts & Rotarians participation make the programe joyous. The Assembly ended by the recognition of Clubs. " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/dist_assembly2.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+												<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/dist_assembly3.jpg" title="28th Dist. Assembly of “We for women 0f today & tomorrow” was held on June 29, 2013 at Palm View Restaurent, Uttara, Dhaka. Eminent personality Syed Shamsul Haq was the Chief Guest. DC 0f 2012-13 Suraiya Alam preside over the meeting. The programme was hosted by IWC of Dhaka North East. The new  team of 2013-14 were warmly welcomed by the outgoing team of 2012-13. IW members from different districts & Rotarians participation make the programe joyous. The Assembly ended by the recognition of Clubs.  " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/dist_assembly3.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+	<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/dist_assembly4.jpg" title="28th Dist. Assembly of “We for women 0f today & tomorrow” was held on June 29, 2013 at Palm View Restaurent, Uttara, Dhaka. Eminent personality Syed Shamsul Haq was the Chief Guest. DC 0f 2012-13 Suraiya Alam preside over the meeting. The programme was hosted by IWC of Dhaka North East. The new  team of 2013-14 were warmly welcomed by the outgoing team of 2012-13. IW members from different districts & Rotarians participation make the programe joyous. The Assembly ended by the recognition of Clubs. " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/dist_assembly4.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              	<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/dua_mahfil1.jpg" title="A dua mahfil was arranged by IWD 328 on 1st July, 2013 for the upcoming IW year 2013-14. The dua was conduct by Past  IIWR/Board member Prof Asma Abbasi & Past NR Razia Sultana. A great number of members from different clubs join the dua for a prosperous year.  " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/dua_mahfil1.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              	<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/dua_mahfil2.jpg" title="A dua mahfil was arranged by IWD 328 on 1st July, 2013 for the upcoming IW year 2013-14. The dua was conduct by Past  IIWR/Board member Prof Asma Abbasi & Past NR Razia Sultana. A great number of members from different clubs join the dua for a prosperous year.  " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/dua_mahfil2.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              	<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/rally1.jpg" title="IW Dist. Rally of ‘Women to change: yes, we can’- hosted by 13 Clubs. On 4th October, 2013 Rally was held at Uttara Club, Dhaka with a festive mood. Renowned Cultural activist Laila Hasan has ornamented the programe as Chief Guest. Decorated with beautifully,  arrangement of different cultural programe with a huge number of audience, stalls, dress as you like- all these  has turned  the Rally a attractive event.  " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/rally1.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              	<div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/rally2.jpg" title="IW Dist. Rally of ‘Women to change: yes, we can’- hosted by 13 Clubs. On 4th October, 2013 Rally was held at Uttara Club, Dhaka with a festive mood. Renowned Cultural activist Laila Hasan has ornamented the programe as Chief Guest. Decorated with beautifully,  arrangement of different cultural programe with a huge number of audience, stalls, dress as you like- all these  has turned  the Rally a attractive event.  " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/rally2.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              <div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/rally3.jpg" title="IW Dist. Rally of ‘Women to change: yes, we can’- hosted by 13 Clubs. On 4th October, 2013 Rally was held at Uttara Club, Dhaka with a festive mood. Renowned Cultural activist Laila Hasan has ornamented the programe as Chief Guest. Decorated with beautifully,  arrangement of different cultural programe with a huge number of audience, stalls, dress as you like- all these  has turned  the Rally a attractive event.  " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/rally3.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              <div id="ngg-image-104" class="ngg-gallery-thumbnail-box"  >
+													<div class="ngg-gallery-thumbnail" >
+														<a href="pg_image/workshop.jpg" title="A professional workshop for the new board of 2013-14 was arranged by IWD 328 on 28 June, 2013 at Gulshan Ladies Club. DC Hosneara Chowdhury PHF preside over the programe.  Members of the Dist. EC Committee as well as the Club EC Committee were present on the workshop. Duties & responsibilities of the committee members were explained by the dignitaries of IW.   " class="thickbox" rel="set_8" > <img title="19" alt="19" src="pg_image/workshop.jpg" width="120" height="90" /> </a>													</div>
+						    </div>
+                                              
+
+								
+                                
+                                
+                                
+                                
+                                
+                                
+		
+									<!-- 26 queries. 0.289 seconds. -->
+									<div>
+									<script type="text/javascript">tb_pathToImage = "g_css/loadingAnimation.gif";tb_closeImage = "g_css/tb-close.png";</script>
+									
+									<script type='text/javascript'>
+									/* <![CDATA[ */
+									var thickboxL10n = {"next":"Next >","prev":"< Prev","image":"Image","of":"of","close":"Close","noiframes":"This feature requires inline frames. You have iframes disabled or your browser does not support them.","loadingAnimation":"http:\/\/linktechbd.com\/wp-includes\/js\/thickbox\/loadingAnimation.gif","closeImage":"http:\/\/linktechbd.com\/wp-includes\/js\/thickbox\/tb-close.png"};
+									/* ]]> */
+									</script> <script type='text/javascript' src='g_css/thickbox9d46.js?ver=3.1-20111117'></script>
+									</div>
+									</body>
+
+									
+</html>
