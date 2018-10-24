f13d5d658fc65a9289e1aab33ad7ec2b336654b9
Bablu Ahmed <bablukpik@gmail.com>
2018-10-25 04:42:52 +0600

    static site of node added

--- nodejs/nodejs.js
+++ nodejs/nodejs.js
@@ -1,3 +1,11 @@
+/*
+var requestListener = function(request, response){
+	response.writeHead(200, {"content-type":"text/html"});
+	response.write("<h1>Alhamdulillah</h1>");
+	response.end();
+};
+*/
+
 var http = require("http");
 var about = require("./bab");
 var port = 3000;
