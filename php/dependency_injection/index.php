09c74bba1fb4601a860d287f3eb5b7bf7e5c837e
Bablu Ahmed <bablukpik@gmail.com>
2018-11-16 20:12:46 +0600

    dependency injection addded

---
+++ php/dependency_injection/index.php
@@ -0,0 +1,69 @@
+<?php
+//dependency class
+class Logger 
+{
+	public function log($message)
+	{
+		echo $message;
+	}
+}
+
+//This class is dependent on Logger class (dependent class)
+//Dependency Injection is also called DRY(Don't Repeat Yourself)
+class UserProfile
+{
+	private $logger;
+	public function __construct(Logger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+	public function createUser()
+	{
+		$this->logger->log("User Created");
+	}
+	
+	public function updateUser()
+	{
+		$this->logger->log("User Updated");
+	}
+
+	public function deleteUser()
+	{
+		$this->logger->log("User Deleted");
+	}
+	
+}
+
+$userProfile = new UserProfile(new Logger);
+$userProfile->createUser();
+
+
+
+/*
+//This class is dependent on Logger class (dependent class)
+class UserProfile
+{
+	public function createUser()
+	{
+		$logger = new Logger();
+		$logger->log("User Created");
+	}
+	
+	public function updateUser()
+	{
+		$logger = new Logger();
+		$logger->log("User Updated");
+	}
+
+	public function deleteUser()
+	{
+		$logger = new Logger();
+		$logger->log("User Deleted");
+	}
+	
+}
+
+$userProfile = new UserProfile();
+$userProfile->createUser();
+*/
