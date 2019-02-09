6dcc574995bca0895c0a2317c0dd40ff7f7522a7
Bablu Ahmed <bablukpik@gmail.com>
2019-02-09 17:46:03 +0600

    sql query added

---
+++ Report/Print/works/promotion_letter_print.php
@@ -0,0 +1,389 @@
+<div class="print_area">
+    <style type="text/css">
+        @import url("<?php echo base_url('dist/styles/main.css'); ?>") print;
+    </style>
+
+    <style type="text/css">
+        @media print, screen {
+            body {
+                background: rgb(204, 204, 204);
+                font-size: 11pt;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+
+            p {
+                margin: 0 0 10px;
+            }
+
+            table {
+                font-family: Arial, sans-serif;
+                font-size: 11pt;
+                border-collapse: collapse;
+                width: 100%;
+                border-spacing: 0;
+            }
+
+            .clearfix::after {
+                display: block;
+                content: "";
+                clear: both;
+            }
+
+            .tab-5 {
+                padding-left: 16px;
+            }
+
+            .tab1 {
+                padding-left: 32px;
+            }
+
+            .tab1-5 {
+                padding-left: 48px;
+            }
+
+            .tab2 {
+                padding-left: 64px;
+            }
+
+            .tab2-5 {
+                padding-left: 112px;
+            }
+
+            .tab3 {
+                padding-left: 128px;
+            }
+
+            .tab3-5 {
+                padding-left: 240px;
+            }
+
+            .tab4 {
+                padding-left: 256px;
+            }
+
+            .tab5 {
+                padding-left: 512px;
+            }
+
+            .lock-header {
+                position: absolute;
+                top: 5px;
+                right: 0;
+                left: 0;
+                text-align: center;
+            }
+
+            .lock-footer {
+                position: absolute;
+                bottom: 5px;
+                right: 0;
+                left: 0;
+                text-align: center;
+            }
+
+            .table td, table th {
+                border: 1px solid black;
+            }
+
+            .page {
+                background: white;
+                display: block;
+                margin: 0 auto;
+                margin-bottom: 0.5cm;
+                box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+                position: relative;
+                padding: .5in .5in .5in .5in;
+                box-sizing: border-box;
+                page-break-after: always;
+                word-wrap: break-word
+            }
+
+            .page[size="A4"][layout="portrait"] {
+                width: 8.5in;
+                height: 11.0in;
+            }
+
+            .page[size="A4"][layout="landscape"] {
+                width: 11.0in;
+                height: 8.5in;
+            }
+
+            .page[size="legal"][layout="portrait"] {
+                width: 8.5in;
+                height: 14.0in;
+            }
+
+            .page[size="legal"][layout="landscape"] {
+                width: 14.0in;
+                height: 8.5in;
+            }
+
+            .table > thead > tr > th, .table > thead > tr > td, .table > tbody > tr > th, .table > tbody > tr > td, .table > tfoot > tr > th, .table > tfoot > tr > td {
+                border: none;
+            }
+
+            @media print {
+                body, .page {
+                    margin: 0;
+                    box-shadow: none;
+                    color: #000;
+                    background-color: #fff;
+                    padding: .2in 0 1.0in;
+                    */ page-break-after: always !important;
+                    page-break-inside: avoid !important;
+                    page-break-before: always !important;
+                }
+
+                .noprint {
+                    display: none;
+                }
+
+                .lock-header {
+                    position: fixed;
+                    top: 0;
+                    right: 0;
+                    left: 0;
+                    text-align: center;
+                }
+
+                .lock-footer {
+                    position: fixed;
+                    bottom: 0;
+                    right: 0;
+                    left: 0;
+                    text-align: center;
+                }
+            }
+    </style>
+
+    <div class="page" size="legal" layout="portrait">
+        <div class="lock-header"><h5 style="font-weight: normal;">CONFIDENTIAL</h5></div>
+        <div class="col-sm-12">
+            <table class="table">
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 150px; float: right;">
+                            <p>
+                                F (SP)-29 <br>
+                                BN-52 (Old)
+                            </p>
+
+                            <p>
+                                Drafting Authority
+                                Naval Headquarters
+                                Banani, Dhaka-1213
+                            </p>
+
+                            <p>Tel : 9836141-9 Ext: 2053</p>
+
+                            <p>03 Poush 1425</p>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>23.02.2626.103.13.009.18.2505</td>
+                    <td>
+                        <div style="width: 150px; float: right;">
+                            <p>17 December 2018</p>
+                        </div>
+                    </td>
+                <tr>
+
+                <tr>
+                    <td colspan="2">
+                        <h4 style="margin-top: 20px; text-decoration: underline; text-transform: uppercase"><strong>PROMOTION/ADVANCEMENT - MCPO/SCPO/CPO/PO/LDG/MODC(N)</strong></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <div style="text-align: justify;">
+                            <p>1. <span class="tab1"></span> O No : <strong style="text-decoration: underline;">20090266</strong> <span class="tab1"></span> Name : <strong style="text-decoration: underline;">Md Imran
+                                    Hossain</strong> <span class="tab2-5" style="padding-left: 190px;"></span> Rank: <span class="tab1"></span> <strong style="text-decoration: underline;">CPO (FC-I)</strong> is to be
+                                promoted/advanced wef. 01-07-2019 and FSP-29 is attached herewith for further necessary actions. Following criteria’s must be fulfilled:</p>
+                            <ol type="A">
+                                <li><span class="tab1"></span> Recommended from Commanding Officer.</li>
+                                <li><span class="tab1"></span> Requirement Good Conduct is fulfilled and free from venereal diseases (vide NR art -0977).</li>
+                                <li><span class="tab1"></span> Medical Category (vide FO-3/95).</li>
+                            </ol>
+
+                            <table style="text-align: center; margin-bottom: 20px;">
+                                <tr>
+                                    <td>O No</td>
+                                    <td>Name</td>
+                                    <td>Promoted Rank</td>
+                                    <td>Wef.</td>
+                                    <td>Date of Seniority</td>
+                                </tr>
+                                <tr>
+                                    <th>20090266</th>
+                                    <th>Md Imran Hossain</th>
+                                    <th>SCPO(X) (FC-I)</th>
+                                    <th>01-07-2019</th>
+                                    <th>01-01-2019</th>
+                                </tr>
+                            </table>
+
+                            <p>2. <span class="tab1"></span> If found unsuitable for Promotion/Advancement, attached F(SP)-29 (2nd copy) is to be returned to this office with detail reasons mentioning the fresh date of
+                                advancement vide NR art – 0980(2).</p>
+
+                            <p>3. <span class="tab1"></span> Promotion/Advancement of Regulating, Technical and other branches are to be guided respectively by NI-1/91, NI-1/S/65, NI-7/S/47, NI-1/2006, FO-16/82 and
+                                FO-08/2008, for the rank of Leading/PO/CPO and SCPO.</p>
+
+                            <p>4. <span class="tab1"></span> Promotion/Advancement may be considered on completion of necessary sea service.</p>
+
+                            <p>5. <span class="tab1"></span> Promotion/Advancement may be considered on reduction of overweight within 1/2 months.</p>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 200px; margin-top: 50px; font-size: 14px; float: right">
+                            <p style="text-transform: uppercase;">AHMED SABBIR NAIHAN <br>
+                                Commander BN <br>
+                                For Drafting Authority</p>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" style="text-align: left;">
+                        <p>Enclosure:</p>
+                        <p>1. <span class="tab1"></span> F(SP)-29 of 2nd copy <span class="tab-5"></span> - 01 (one) page.</p>
+                        <p>Distribution:</p>
+                        <p>External:</p>
+                        <p>Action:</p>
+                        <p>Commanding Officer <br>
+                            BNS ALI HAIDER</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" style="font-style: italic;">
+                        <p><strong>NB: Pl cut through accordingly.</strong></p>
+                        <span style="font-size: 10px;">BNCPP – 433 (R)</span>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="lock-footer"><h5 style="font-weight: normal;">CONFIDENTIAL</h5></div>
+    </div>
+
+    <div class="page" size="legal" layout="portrait">
+        <div class="lock-header noprint"><h5 style="font-weight: normal;">CONFIDENTIAL</h5></div>
+        <br>
+        <div class="col-sm-12">
+            <table class="table">
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 300px; float: right;">
+                            <p>
+                                F(SP)-29 (2nd Copy) <br>
+                                BN-52 (Old)
+                            </p>
+
+                            <div style="word-wrap: break-word;">
+                                <p>
+                                    BNS....................................................................................................................................................................................................................</p>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 280px; float: left;">
+                            <p>No..............................................</p>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 150px; float: left;">
+                            <p>
+                                Drafting Authority
+                                Naval Headquarters
+                                Banani, Dhaka-1213
+                            </p>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <p style="margin-top: 20px;">PROMOTION/ADVANCEMENT: <span class="tab-5"></span> O No: <strong style="text-decoration: underline;">20090266</strong> <span class="tab-5"></span> Name:
+                            <strong style="text-decoration: underline">Md Imran Hossain</strong> <span class="tab1"></span>Rank: <strong style="text-decoration: underline;">CPO (FC-I)</strong></p>
+                        <p style="margin-top: 20px;">Ref:</p>
+                        <p style="margin-top: 20px; margin-bottom: 20px;">A. <span class="tab1"></span> Drafting Authority ltr no 23.02.2626.103.12.009.18.2505 dated 17 December 2018.</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <h4><strong style="text-decoration: underline;">Medical Officer’s Remarks</strong></h4>
+                        <div style="text-align: justify;">
+                            <p>1. Vide FO-17/16 para-4, NI-1/2006 para 7(Ga), NI-7/S/47 para-7, NI-1/65 para-5(B) and FO 08/2008 para-23 above mentioned SCPO/CPO/PO/SAILOR/MODC(N) is medically fit/unfit.</p>
+                            <p style="text-align: center; vertical-align: middle;">or</p>
+                            <p>Promotion/Advancement is not awarded above mentioned SCPO/CPO/PO/SAILOR/MODC(N). It is recommended for his Promotion/Advancement after……………………. months.</p>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 300px; float: right;">
+                            <p style="margin: 0;">Signature <span class="tab-5" style="padding-left: 20px;"></span>: </p>
+                            <p style="margin: 0;">.........................................................................</p>
+                            <p style="margin: 0;">Name <span class="tab1" style="padding-left: 44px;"></span> :</p>
+                            <p style="margin: 0;">.........................................................................</p>
+                            <p style="margin: 0;">Rank <span class="tab1-5"></span>:</p>
+                            <p style="margin: 0;">.........................................................................</p>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <h4><strong style="text-decoration: underline;">Commanding Officer’s Remark</strong></h4>
+                        <div style="text-align: justify;">
+                            <p>1. <span class="tab1"></span> Above mentioned SCPO/CPO/PO/SAILOR/MODC(N) is fully qualified for Promotion/
+                                Advancement and Promotion/Advancement has been awarded wef.................................. as per
+                                ref A. Attached with GXS.............................. Dated............................ for published DAO.</p>
+                            <p style="text-align: center; vertical-align: middle;">or</p>
+                            <p>Promotion/Advancement is not awarded above mentioned SCPO/CPO/PO/SAILOR/MODC(N). It is
+                                recommended for his Promotion/Advancement after......................... months.</p>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div style="width: 300px; float: right;">
+                            <p style="margin: 0;">Signature <span class="tab-5" style="padding-left: 20px;"></span>: </p>
+                            <p style="margin: 0;">.........................................................................</p>
+                            <p style="margin: 0;">Name <span class="tab1" style="padding-left: 44px;"></span> :</p>
+                            <p style="margin: 0;">.........................................................................</p>
+                            <p style="margin: 0;">Rank <span class="tab1-5"></span>:</p>
+                            <p style="margin: 0;">.........................................................................</p>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" style="text-align: left;">
+                        <p><strong>Encl:</strong></p>
+                        <p>1. GXS <span style="padding-right: 133px;"></span> - 01 (One) Copy.</p>
+                        <p>2. New Rank Photo <span class="tab1"></span> - 01 (One) Copy.</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <p style="font-style: italic;"><strong>NB: Pl cut through accordingly.</strong></p>
+                        <span style="font-size: 10px;">BNCPP – 433 (R)</span>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="lock-footer noprint"><h5 style="font-weight: normal;">CONFIDENTIAL</h5></div>
+    </div>
+</div>
