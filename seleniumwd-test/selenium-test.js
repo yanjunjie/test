1b06dbf82181252eb57a06b1b19d44e970be2a9e
Bablu Ahmed <bablukpik@gmail.com>
2019-07-22 13:22:02 +0600

    selenium wd note added

--- seleniumwd-test/selenium-test.js
+++ seleniumwd-test/selenium-test.js
@@ -1,13 +1,13 @@
 let webdriver = require('selenium-webdriver');
-webdriver.Browser // to see the availabel list of browsers
+// webdriver.Browser // to see the availabel list of browsers
 let driver = new webdriver.Builder().forBrowser('chrome').build();
 driver.get('http://www.google.com'); // go to google.com
 driver.findElement(webdriver.By.name('q')).sendKeys('Hello World');
 //driver.findElement({name: 'q'}).sendKeys('webdriverjs');
-driver.findElement(webdriver.By.className('gNO89b')).click();
+driver.findElement({ xpath: '(//input[@class="gNO89b"])[2]' }).click();
+//driver.findElement(webdriver.By.className('gNO89b')).click();
 //driver.findElement({name: 'q'}).sendKeys(webdriver.key.ENTER);
-
-//driver.wait(webdriver.until.elementLocated({ xpath: '//adf' }, 1000));
-//driver.findElement({ xpath:'//adf' }).click();
-//driver.getTitle().then(title=>console.log(title));
+driver.wait(webdriver.until.elementLocated({ xpath: `//h3[text()='"Hello, World!" program - Wikipedia']` }, 1000));
+driver.findElement({ xpath:`//h3[text()='"Hello, World!" program - Wikipedia']` }).click();
+driver.getTitle().then(title=>console.log(title));
 //driver.quit();
