42028ab074593adb438bfea6d240d5c3ea9729c8
Bablu Ahmed <bablukpik@gmail.com>
2020-01-14 01:14:54 +0600

    constraint layout project added

--- Android/Projects/AllLayouts/app/src/main/java/com/example/alllayouts/ConstraintLayoutTestV2.java
+++ Android/Projects/AllLayouts/app/src/main/java/com/example/alllayouts/ConstraintLayoutTestV2.java
@@ -1,14 +1,21 @@
 package com.example.alllayouts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Placeholder;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ConstraintLayoutTestV2 extends AppCompatActivity {
-
+    private Placeholder placeholder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout_test_v2);
+        placeholder = findViewById(R.id.placeholder);
+    }
+
+    public void swapView(View v) {
+        placeholder.setContentId(v.getId());
     }
 }
