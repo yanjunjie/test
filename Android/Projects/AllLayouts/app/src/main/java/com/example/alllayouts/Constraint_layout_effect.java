9a85a9fa27a958760d14c41527975a87bdf18f4a
Bablu Ahmed <bablukpik@gmail.com>
2020-01-22 01:50:22 +0600

    node-cli note added

--- Android/Projects/AllLayouts/app/src/main/java/com/example/alllayouts/Constraint_layout_effect.java
+++ Android/Projects/AllLayouts/app/src/main/java/com/example/alllayouts/Constraint_layout_effect.java
@@ -1,14 +1,44 @@
 package com.example.alllayouts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.transition.ChangeBounds;
+import androidx.transition.Transition;
+import androidx.transition.TransitionManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 public class Constraint_layout_effect extends AppCompatActivity {
+    private ConstraintLayout layout;
+    private ConstraintSet constraintSetOld = new ConstraintSet();
+    private ConstraintSet constraintSetNew = new ConstraintSet();
+    private boolean altLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout_effect);
+        layout = findViewById(R.id.layout);
+
+        constraintSetOld.clone(layout);
+        constraintSetNew.clone(this, R.layout.activity_constraint_layout_effect_alt);
+    }
+
+    public void swapView(View v) {
+        Transition changeBounds = new ChangeBounds();
+        changeBounds.setInterpolator(new OvershootInterpolator());
+
+        TransitionManager.beginDelayedTransition(layout, changeBounds);
+
+        if (!altLayout) {
+            constraintSetNew.applyTo(layout);
+            altLayout = true;
+        } else {
+            constraintSetOld.applyTo(layout);
+            altLayout = false;
+        }
     }
 }
